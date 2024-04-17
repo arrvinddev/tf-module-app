f29bf1a6bbfa7ab161ff2556e580a1facc7964f7
arrvind <141898022+arrvindops@users.noreply.github.com>
2024-04-17 14:28:10 +0530

    asg

--- data.tf
+++ data.tf
@@ -1,1 +1,5 @@
-data "aws_caller_identity" "identity"{}
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
--- vars.tf
+++ vars.tf
@@ -1,4 +1,13 @@
-variable "cidr_block" {}
-variable "tags"{}
-variable "env" {}
-variable "subnets"{}
+variable "instance_type" {}
+variable "subnet_ids" {}
+variable "name" {}
+variable "env"{}
+variable "vpc_id"{}
+variable "allow_app_cidr" {}
+variable "bastion_cidr"{}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "tags" {
+  
+}
